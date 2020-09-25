d894a73408a8322101777d1e7e4437afa5434fbf
anahi <anahimayancela@gmail.com>
2020-09-25 00:33:04 -0400

    refactoring js code into separate file using event hooking

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() { //using event hooking
+    caseSwitching();
+    function caseSwitching(){
+        let desserts = 'ice-cream';
+            switch(desserts){
+                case 'french-toast':
+                    console.log('French toast in delicious');
+                    document.getElementById('dessert').innerHTML= "<img src='images/french-toast.jpg'>";
+
+				case 'ice-cream':
+					console.log('You could never go wrong with ice cream');
+					document.getElementById('dessert').innerHTML = "<img src='images/ice-cream.jpg'>";
+
+
+			}
+    };
+}
